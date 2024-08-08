443801db7d8238f77b2a8de9931d00e6cdf4bb15
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-08-08 14:55:57 +0300

    create User model

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const { required } = require('joi')
+const mongoose = require('mongoose')
+
+const UserSchema = new mongoose.Schema ({
+    name:{
+        type: String,
+        required: [true, 'Please provide a name'],
+        minlength:3,
+        maxlength:50,
+    },
+    email:{
+        type: String,
+        required:[true, 'Please provide a email'],
+        
+    },
+    password:{
+        type: String,
+        required:[true, 'Please provide a password'],
+        minlength:6
+    },
+    role:{
+        type:String,
+        enum: ['admin', 'user'],
+        default: 'user'
+    },
+})
+
+module.exports = mongoose.model('User',UserSchema)
