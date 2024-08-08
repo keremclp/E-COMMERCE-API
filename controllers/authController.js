ce5f84d9f325b756da10d5ee91559d787417ad68
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-08-08 23:37:42 +0300

    ignore role and admin setup

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,12 +6,17 @@ const login = (req,res) =>{
     res.send('Login')
 }
 const register = async (req,res) =>{
-    const { email } = req.body
+    const { email,name,password } = req.body
+
     const emailAlreadyExist = await User.findOne({ email })
     if(emailAlreadyExist){
         throw new CustomError.BadRequestError('Email already exist')
     }
-    const user = await User.create(req.body)
+
+    // first registered user is an admin
+    const isFirstAccount = await User.countDocuments({}) === 0;
+    const role = isFirstAccount ? 'admin' : 'user'
+    const user = await User.create({ name,email,password,role }) // for security, we can only change on MongoDB
     res.status(StatusCodes.CREATED).json({ user })
 }
 const logout = (req,res) =>{
