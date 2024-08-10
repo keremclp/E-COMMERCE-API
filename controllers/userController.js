156a9b9d369b56226374724b8a3980a061fb2cfd
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-08-10 16:42:51 +0300

    get all user and get single user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,23 @@
-    const getAllUser = async (req,res) =>{
-        res.send('getAllUser')
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
+const CustomError = require('../errors')
+const getAllUser = async (req,res) =>{
+    // remove the password by select func
+    const users = await User.find({ role:'user' }).select('-password')
+    if(!users){
+        throw new CustomError.NotFoundError("No user found");
     }
+    users.forEach(user => delete user.password)
+    res.status(StatusCodes.OK).json({ user:users })
+}
 const getSingleUser = async (req,res) =>{
-    res.send('getSingleUser')
+    const userId = req.params.id
+    const user = await User.findOne({ _id: userId }).select('-password')
+    if(!user){
+        throw new CustomError.NotFoundError("User not found");
+    }
+
+    res.status(StatusCodes.OK).json({ user })
 }
 const showCurrentUser = async (req,res) =>{
     res.send('showCurrentUser')
