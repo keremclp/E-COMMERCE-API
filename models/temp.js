5a272c8605eca5f6a55e1d55d627b7f70b9bb983
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-08-15 18:11:43 +0300

    aggregation pipeline - Atlas

---
+++ models/temp.js
@@ -0,0 +1,32 @@
+import { MongoClient } from "mongodb";
+import { ObjectId } from "mongodb";
+
+/*
+ * Requires the MongoDB Node.js Driver
+ * https://mongodb.github.io/node-mongodb-native
+ */
+
+const agg = [
+  {
+    $match: {
+      product: new ObjectId("66be15b422d47b06ca580c1d"),
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      averageRating: {
+        $avg: "$rating",
+      },
+      numOfReviews: {
+        $sum: 1,
+      },
+    },
+  },
+];
+
+const client = await MongoClient.connect("");
+const coll = client.db("e-commerce-api").collection("reviews");
+const cursor = coll.aggregate(agg);
+const result = await cursor.toArray();
+await client.close();
