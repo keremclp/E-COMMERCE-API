c21f8dffd015a2c70ed4ccc88314541cc1218934
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-08-17 14:02:58 +0300

    create product controller setup- checking product

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,5 +1,46 @@
+const Order = require("../models/Order");
+const Product = require("../models/Product");
+
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+const { checkPermissions } = require("../utils");
+
 const createOrder = async (req,res) =>{
-    res.send("createOrder");
+    const {items:cartItems, tax, shippingFee} = req.body;
+
+    if(!cartItems || cartItems.length <1){
+        throw new CustomError.BadRequestError("No items in cart");
+    }
+
+    if(!tax || !shippingFee){
+        throw new CustomError.BadRequestError("Please provide tax and shipping fee");
+    }
+
+    let orderItems = []
+    let subtotal = 0;
+    
+    for(const item of cartItems){
+        const dbProduct = await Product.findOne({_id:item.product}); // check the orders.json
+        if(!dbProduct){
+            throw new CustomError.NotFoundError(`No product with ID: ${item.product}`);
+        }
+        const { name, price, image, _id } = dbProduct
+        // console.log(name, price, image, _id);
+        
+
+        const singleOrder = {
+            amount: item.amount,
+            name,
+            price,
+            image,
+            product:_id,
+        };
+        orderItems = [...orderItems, singleOrder] // add item to order 
+        subtotal += price * item.amount; // calculate subtotal
+    }
+    // console.log(orderItems);
+    // console.log(subtotal);
+    res.send('createOrder')
 }
 const getAllOrders = async (req,res) =>{
     res.send("getAllOrders");
