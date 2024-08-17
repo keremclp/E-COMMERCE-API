23ca71a2e405e9df7152d25b2264610e0240994a
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-08-17 17:07:02 +0300

    docgen docs html, security packgaes

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 require('dotenv').config()
 require('express-async-errors')
 
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors')
+const rateLimit = require('express-rate-limit');
+const xss = require('xss-clean')
+const mongoSanitize = require('express-mongo-sanitize')
+
+
 // express 
 const express = require('express')
 const app = express()
@@ -21,6 +29,17 @@ const connectDB = require('./db/connect')
 // error handler
 const notFoundMiddlewear = require('./middleware/not-found')
 const errorHandlerMiddlewear = require('./middleware/error-handler')
+
+// extra packages
+app.set('trust proxy',1)
+app.use(rateLimit( {
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 60 // limit each IP to 100 requests per windowMs
+}))
+app.use(helmet());
+app.use(cors());
+app.use(xss())
+app.use(mongoSanitize())
 
 // middlewear
 app.use(morgan('tiny'))
